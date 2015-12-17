ab1c5653461b3f3e6cc6503aa454d10287b6a467
Miguel Crespo <miguel.crespo6@gmail.com>
2015-12-17 00:40:54 -0500

    Setting up grunt connect to serve the page without use apache or another server...

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,158 +8,219 @@
 
 module.exports = function (grunt) {
 
-  'use strict';
+    'use strict';
 
-  grunt.initConfig({
-    watch: {
-      // If any .less file changes in directory "build/less/" run the "less"-task.
-      files: ["build/less/*.less", "build/less/skins/*.less", "dist/js/app.js"],
-      tasks: ["less", "uglify"]
-    },
-    // "less"-task configuration
-    // This task will compile all less files upon saving to create both AdminLTE.css and AdminLTE.min.css
-    less: {
-      // Development not compressed
-      development: {
-        options: {
-          // Whether to compress or not
-          compress: false
-        },
-        files: {
-          // compilation.css  :  source.less
-          "dist/css/AdminLTE.css": "build/less/AdminLTE.less",
-          //Non minified skin files
-          "dist/css/skins/skin-blue.css": "build/less/skins/skin-blue.less",
-          "dist/css/skins/skin-black.css": "build/less/skins/skin-black.less",
-          "dist/css/skins/skin-yellow.css": "build/less/skins/skin-yellow.less",
-          "dist/css/skins/skin-green.css": "build/less/skins/skin-green.less",
-          "dist/css/skins/skin-red.css": "build/less/skins/skin-red.less",
-          "dist/css/skins/skin-purple.css": "build/less/skins/skin-purple.less",
-          "dist/css/skins/skin-blue-light.css": "build/less/skins/skin-blue-light.less",
-          "dist/css/skins/skin-black-light.css": "build/less/skins/skin-black-light.less",
-          "dist/css/skins/skin-yellow-light.css": "build/less/skins/skin-yellow-light.less",
-          "dist/css/skins/skin-green-light.css": "build/less/skins/skin-green-light.less",
-          "dist/css/skins/skin-red-light.css": "build/less/skins/skin-red-light.less",
-          "dist/css/skins/skin-purple-light.css": "build/less/skins/skin-purple-light.less",
-          "dist/css/skins/_all-skins.css": "build/less/skins/_all-skins.less"
-        }
-      },
-      // Production compresses version
-      production: {
-        options: {
-          // Whether to compress or not
-          compress: true
-        },
-        files: {
-          // compilation.css  :  source.less
-          "dist/css/AdminLTE.min.css": "build/less/AdminLTE.less",
-          // Skins minified
-          "dist/css/skins/skin-blue.min.css": "build/less/skins/skin-blue.less",
-          "dist/css/skins/skin-black.min.css": "build/less/skins/skin-black.less",
-          "dist/css/skins/skin-yellow.min.css": "build/less/skins/skin-yellow.less",
-          "dist/css/skins/skin-green.min.css": "build/less/skins/skin-green.less",
-          "dist/css/skins/skin-red.min.css": "build/less/skins/skin-red.less",
-          "dist/css/skins/skin-purple.min.css": "build/less/skins/skin-purple.less",
-          "dist/css/skins/skin-blue-light.min.css": "build/less/skins/skin-blue-light.less",
-          "dist/css/skins/skin-black-light.min.css": "build/less/skins/skin-black-light.less",
-          "dist/css/skins/skin-yellow-light.min.css": "build/less/skins/skin-yellow-light.less",
-          "dist/css/skins/skin-green-light.min.css": "build/less/skins/skin-green-light.less",
-          "dist/css/skins/skin-red-light.min.css": "build/less/skins/skin-red-light.less",
-          "dist/css/skins/skin-purple-light.min.css": "build/less/skins/skin-purple-light.less",
-          "dist/css/skins/_all-skins.min.css": "build/less/skins/_all-skins.less"
-        }
-      }
-    },
-    // Uglify task info. Compress the js files.
-    uglify: {
-      options: {
-        mangle: true,
-        preserveComments: 'some'
-      },
-      my_target: {
-        files: {
-          'dist/js/app.min.js': ['dist/js/app.js']
-        }
-      }
-    },
-    // Build the documentation files
-    includes: {
-      build: {
-        src: ['*.html'], // Source files
-        dest: 'documentation/', // Destination directory
-        flatten: true,
-        cwd: 'documentation/build',
-        options: {
-          silent: true,
-          includePath: 'documentation/build/include'
-        }
-      }
-    },
+    var serveStatic = require('serve-static');
 
+    grunt.initConfig({
+        connect: {
+            server: {
+                options: {
+                    port: 9000,
+                    base: '',
+                    //keepalive: true,
+                    // Livereload needs connect to insert a cJavascript snippet
+                    // in the pages it serves. This requires using a custom connect middleware
+                    middleware: function (connect, options) {
+
+                        return [
+
+                            // Load the middleware provided by the livereload plugin
+                            // that will take care of inserting the snippet
+                            require('grunt-contrib-livereload/lib/utils').livereloadSnippet,
+
+                            // Serve the project folder
+                            connect.static(options.base)
+                        ];
+                    }
+                }
+            }
+        },
+        // grunt-open will open your browser at the project's URL
+        open: {
+            all: {
+                // Gets the port from the connect configuration
+                path: 'http://localhost:<%= connect.server.options.port%>'
+            }
+        },
+        // grunt-regarde monitors the files and triggers livereload
+        // Surprisingly, livereload complains when you try to use grunt-contrib-watch instead of grunt-regarde
+        regarde: {
+            all: {
+                // This'll just watch the index.html file, you could add **/*.js or **/*.css
+                // to watch Javascript and CSS files too.
+                files: ['index.html'],
+                // This configures the task that will run when the file change
+                tasks: ['livereload']
+            }
+        },
+        watch: {
+            // If any .less file changes in directory "build/less/" run the "less"-task.
+            files: ["build/less/*.less", "build/less/skins/*.less", "dist/js/app.js"],
+            tasks: ["less", "uglify"]
+        },
+        // "less"-task configuration
+        // This task will compile all less files upon saving to create both AdminLTE.css and AdminLTE.min.css
+        less: {
+            // Development not compressed
+            development: {
+                options: {
+                    // Whether to compress or not
+                    compress: false
+                },
+                files: {
+                    // compilation.css  :  source.less
+                    "dist/css/AdminLTE.css": "build/less/AdminLTE.less",
+                    //Non minified skin files
+                    "dist/css/skins/skin-blue.css": "build/less/skins/skin-blue.less",
+                    "dist/css/skins/skin-black.css": "build/less/skins/skin-black.less",
+                    "dist/css/skins/skin-yellow.css": "build/less/skins/skin-yellow.less",
+                    "dist/css/skins/skin-green.css": "build/less/skins/skin-green.less",
+                    "dist/css/skins/skin-red.css": "build/less/skins/skin-red.less",
+                    "dist/css/skins/skin-purple.css": "build/less/skins/skin-purple.less",
+                    "dist/css/skins/skin-blue-light.css": "build/less/skins/skin-blue-light.less",
+                    "dist/css/skins/skin-black-light.css": "build/less/skins/skin-black-light.less",
+                    "dist/css/skins/skin-yellow-light.css": "build/less/skins/skin-yellow-light.less",
+                    "dist/css/skins/skin-green-light.css": "build/less/skins/skin-green-light.less",
+                    "dist/css/skins/skin-red-light.css": "build/less/skins/skin-red-light.less",
+                    "dist/css/skins/skin-purple-light.css": "build/less/skins/skin-purple-light.less",
+                    "dist/css/skins/_all-skins.css": "build/less/skins/_all-skins.less"
+                }
+            },
+            // Production compresses version
+            production: {
+                options: {
+                    // Whether to compress or not
+                    compress: true
+                },
+                files: {
+                    // compilation.css  :  source.less
+                    "dist/css/AdminLTE.min.css": "build/less/AdminLTE.less",
+                    // Skins minified
+                    "dist/css/skins/skin-blue.min.css": "build/less/skins/skin-blue.less",
+                    "dist/css/skins/skin-black.min.css": "build/less/skins/skin-black.less",
+                    "dist/css/skins/skin-yellow.min.css": "build/less/skins/skin-yellow.less",
+                    "dist/css/skins/skin-green.min.css": "build/less/skins/skin-green.less",
+                    "dist/css/skins/skin-red.min.css": "build/less/skins/skin-red.less",
+                    "dist/css/skins/skin-purple.min.css": "build/less/skins/skin-purple.less",
+                    "dist/css/skins/skin-blue-light.min.css": "build/less/skins/skin-blue-light.less",
+                    "dist/css/skins/skin-black-light.min.css": "build/less/skins/skin-black-light.less",
+                    "dist/css/skins/skin-yellow-light.min.css": "build/less/skins/skin-yellow-light.less",
+                    "dist/css/skins/skin-green-light.min.css": "build/less/skins/skin-green-light.less",
+                    "dist/css/skins/skin-red-light.min.css": "build/less/skins/skin-red-light.less",
+                    "dist/css/skins/skin-purple-light.min.css": "build/less/skins/skin-purple-light.less",
+                    "dist/css/skins/_all-skins.min.css": "build/less/skins/_all-skins.less"
+                }
+            }
+        },
+        // Uglify task info. Compress the js files.
+        uglify: {
+            options: {
+                mangle: true,
+                preserveComments: 'some'
+            },
+            my_target: {
+                files: {
+                    'dist/js/app.min.js': ['dist/js/app.js']
+                }
+            }
+        },
+        // Build the documentation files
+        includes: {
+            build: {
+                src: ['*.html'], // Source files
+                dest: 'documentation/', // Destination directory
+                flatten: true,
+                cwd: 'documentation/build',
+                options: {
+                    silent: true,
+                    includePath: 'documentation/build/include'
+                }
+            }
+        },
+
+        // Optimize images
+        image: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'build/img/',
+                    src: ['**/*.{png,jpg,gif,svg,jpeg}'],
+                    dest: 'dist/img/'
+                }]
+            }
+        },
+
+        // Validate JS code
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            core: {
+                src: 'dist/js/app.js'
+            },
+            demo: {
+                src: 'dist/js/demo.js'
+            },
+            pages: {
+                src: 'dist/js/pages/*.js'
+            }
+        },
+
+        csslint: {
+            options: {
+                csslintrc: 'build/less/.csslintrc'
+            },
+            dist: [
+                'dist/css/AdminLTE.css',
+            ]
+        },
+
+        // Delete images in build directory
+        // After compressing the images in the build/img dir, there is no need
+        // for them
+        clean: {
+            build: ["build/img/*"]
+        }
+    });
+
+    // Load all grunt tasks
+
+    // LESS Compiler
+    grunt.loadNpmTasks('grunt-contrib-less');
+    // Watch File Changes
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    // Compress JS Files
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    // Include Files Within HTML
+    grunt.loadNpmTasks('grunt-includes');
     // Optimize images
-    image: {
-      dynamic: {
-        files: [{
-          expand: true,
-          cwd: 'build/img/',
-          src: ['**/*.{png,jpg,gif,svg,jpeg}'],
-          dest: 'dist/img/'
-        }]
-      }
-    },
-
+    grunt.loadNpmTasks('grunt-image');
     // Validate JS code
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      core: {
-        src: 'dist/js/app.js'
-      },
-      demo: {
-        src: 'dist/js/demo.js'
-      },
-      pages: {
-        src: 'dist/js/pages/*.js'
-      }
-    },
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    // Delete not needed files
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    // Lint CSS
+    grunt.loadNpmTasks('grunt-contrib-csslint');
 
-    csslint: {
-      options: {
-        csslintrc: 'build/less/.csslintrc'
-      },
-      dist: [
-        'dist/css/AdminLTE.css',
-      ]
-    },
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-livereload');
+    grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-regarde');
 
-    // Delete images in build directory
-    // After compressing the images in the build/img dir, there is no need
-    // for them
-    clean: {
-      build: ["build/img/*"]
-    }
-  });
 
-  // Load all grunt tasks
+    // Connect
+    grunt.registerTask('server', [
 
-  // LESS Compiler
-  grunt.loadNpmTasks('grunt-contrib-less');
-  // Watch File Changes
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  // Compress JS Files
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  // Include Files Within HTML
-  grunt.loadNpmTasks('grunt-includes');
-  // Optimize images
-  grunt.loadNpmTasks('grunt-image');
-  // Validate JS code
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  // Delete not needed files
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  // Lint CSS
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-
-  // The default task (running "grunt" in console) is "watch"
-  grunt.registerTask('default', ['watch']);
+        // Starts the livereload server to which the browser will connect to
+        // get notified of when it needs to reload
+        'livereload-start',
+        'connect',
+        // Connect is no longer blocking other tasks, so it makes more sense to open the browser after the server starts
+        'open',
+        // Starts monitoring the folders and keep Grunt alive
+        'regarde'
+    ]);
+    // The default task (running "grunt" in console) is "watch"
+    grunt.registerTask('default', ['watch']);
 };
